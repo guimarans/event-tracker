67c74e7b27e115d6f9018a66b7db9e9561e52322
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-03-28 23:08:59 -0300

    refactor: ajuste de codigo com recoil

---
+++ src/state/atom.ts
@@ -0,0 +1,20 @@
+import { IEvento } from "interfaces/IEvento";
+import { atom } from "recoil";
+
+export const listaDeEventosState = atom<IEvento[]>({
+  key: 'ListaDeEventosState',
+  default: [{
+    "descricao": "Estudar React",
+    "inicio": new Date("2022-01-15T09:00"),
+    "fim": new Date("2022-01-15T13:00"),
+    "completo": false,
+    "id": 1642342747
+  },
+  {
+    "descricao": "Estudar Recoil",
+    "inicio": new Date("2022-01-16T09:00"),
+    "fim": new Date("2022-01-16T11:00"),
+    "completo": false,
+    "id": 1642342959
+  }]
+})
---
+++ src/state/hooks/useAtualizarEvento.ts
@@ -0,0 +1,15 @@
+import { listaDeEventosState } from './../atom';
+import { IEvento } from "interfaces/IEvento";
+import { useSetRecoilState } from "recoil";
+
+export default function useAtualizarEvento() {
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+
+  return (evento: IEvento) => {
+    return setListaDeEventos(listaAntiga => {
+      const indice = listaAntiga.findIndex(evt => evt.id === evento.id);
+
+      return [...listaAntiga.slice(0, indice), evento, ...listaAntiga.slice(indice + 1)]
+    })
+  }
+}
